4eb00c9702b6e3e521ff378d2920ba9b335941e0
RobbeLehaen <robbelehaen@gmail.com>
2024-04-18 19:43:46 +0200

    Main screen +DataActivity class

--- app/src/main/java/com/example/warehousemanager/MainActivity.java
+++ app/src/main/java/com/example/warehousemanager/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.warehousemanager;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonOpenData = findViewById(R.id.Search_button);
+        buttonOpenData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DataActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }

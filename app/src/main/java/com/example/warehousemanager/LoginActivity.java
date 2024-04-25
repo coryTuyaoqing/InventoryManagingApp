dd36ccef0648c543dd9e9382a1acaa9650031445
coryTu <202164050406@mail.scut.edu.cn>
2024-04-25 15:52:45 +0200

    LoginActivity.java: add internet permission & get password successfully

--- app/src/main/java/com/example/warehousemanager/LoginActivity.java
+++ app/src/main/java/com/example/warehousemanager/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.warehousemanager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -9,8 +10,13 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.Response;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -35,17 +41,38 @@ public class LoginActivity extends AppCompatActivity {
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String nameOrId = edtTxtNameOrId.getText().toString();
+                String id = edtTxtNameOrId.getText().toString();
                 String password = edtTxtPassword.getText().toString();
-                int id;
-                String name;
-
-                id = Integer.parseInt(nameOrId);
 
                 //set up http request
                 Request request = new Request.Builder()
-                        .url(DBConst.DB_URL + "get_staff_password/")
+                        .url(DBConst.DB_URL + "get_staff_password/" + id)
                         .build();
+
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                        System.out.println("fail to get password");
+                    }
+
+                    @Override
+                    public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                        if(response.isSuccessful()){
+                            String responseData = response.body().string();
+                            System.out.println("success to get password: " + responseData);
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(LoginActivity.this, "success to get password: " + responseData, Toast.LENGTH_LONG).show();
+                                }
+                            });
+                        }
+                        else{
+                            System.out.println("fail to get password: " + response.code());
+                        }
+
+                    }
+                });
             }
         });
     }

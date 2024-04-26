ac9b25a0216a19901d6460509ef1cb9f8d1c1161
RobbeLehaen <robbelehaen@gmail.com>
2024-04-26 17:23:05 +0200

    save button implemented

--- app/src/main/java/com/example/warehousemanager/StaffActivity.java
+++ app/src/main/java/com/example/warehousemanager/StaffActivity.java
@@ -3,19 +3,17 @@ package com.example.warehousemanager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.widget.TableRow;
-import android.widget.TextView;
-import android.widget.Spinner;
-import android.widget.ArrayAdapter;
-import android.view.View;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -25,18 +23,21 @@ import java.io.IOException;
 
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class StaffActivity extends AppCompatActivity {
     private TableLayout tableLayout;
+    private OkHttpClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_staffmanagement);
-        OkHttpClient client = new OkHttpClient();
+        client = new OkHttpClient();
         tableLayout = findViewById(R.id.StaffTable);
         String url = "https://studev.groept.be/api/a23PT308/get_info_Staffs";
 
@@ -73,21 +74,7 @@ public class StaffActivity extends AppCompatActivity {
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                for (int i = 1; i < tableLayout.getChildCount(); i++)
-                {
-                    TableRow row = (TableRow) tableLayout.getChildAt(i);
-
-                    TextView nameTextView = (TextView) row.getChildAt(0);
-                    Spinner permissionSpinner = (Spinner) row.getChildAt(1);
-
-                    String staffName = nameTextView.getText().toString();
-                    String selectedPermission = (String) permissionSpinner.getSelectedItem();
-                    int permissionIndex = permissionSpinner.getSelectedItemPosition() + 1;
-
-                    //implement code to send staff to Database
-                }
-
-                Toast.makeText(StaffActivity.this, "Data saved", Toast.LENGTH_SHORT).show();
+                saveStaffData();
             }
         });
 
@@ -141,5 +128,54 @@ public class StaffActivity extends AppCompatActivity {
         } catch (JSONException e) {
             e.printStackTrace();
         }
+    }
+
+    private void saveStaffData() {
+        String baseUrl = "https://studev.groept.be/api/a23PT308/save_staff/";
+
+        for (int i = 1; i < tableLayout.getChildCount(); i++) {
+            TableRow row = (TableRow) tableLayout.getChildAt(i);
+
+            if (row.getChildCount() >= 3) {
+                Spinner permissionSpinner = (Spinner) row.getChildAt(2); // Adjusted index to access the spinner in the third column
+                int permissionIndex = permissionSpinner.getSelectedItemPosition() + 1;
+
+                TextView idTextView = (TextView) row.getChildAt(0);
+                int id = Integer.parseInt(idTextView.getText().toString());
+
+                String url = baseUrl + permissionIndex + "/" + id;
+
+                Request request = new Request.Builder()
+                        .url(url)
+                        .build();
+
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                        System.out.println("fail to save data");
+                    }
+
+                    @Override
+                    public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                        if (response.isSuccessful()) {
+                            String responseData = response.body().string();
+                            System.out.println("success to save data");
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(StaffActivity.this, "success to save data", Toast.LENGTH_LONG).show();
+                                }
+                            });
+                            populateTable(responseData);
+                        }
+                        else {
+                            System.out.println("fail to save data");
+                        }
+                    }
+                });
+            }
+        }
+
+        Toast.makeText(StaffActivity.this, "Data saved", Toast.LENGTH_SHORT).show();
     }
 }
